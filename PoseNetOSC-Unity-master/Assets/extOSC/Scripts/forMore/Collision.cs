b40b783ad9f7632b9c93d6fb29638775b994b8ef
croyn <batchild2004@hotmail.com>
2020-04-25 19:33:21 +0200

    subida entera
    -Capta cuando coges pikcup. Cuando los recoges todos aparece la esfera.
    
    -La esfera cambia de tamaño segun lo lejos o cerca que estan tus brazos entre si.(en x)
    -Cambia de color segun los alto que tengas LOS DOS BRAZOS.(en y)

---
+++ PoseNetOSC-Unity-master/Assets/extOSC/Scripts/forMore/Collision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject sphere;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            controlScript.instance.setCounter();
+
+            Debug.Log("Aqui tengo " + controlScript.instance.getCounter().ToString());
+            
+            //counter++;
+            // SetCountText();
+        }
+
+
+    }
+
+}
